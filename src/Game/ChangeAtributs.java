c5807352c06e989c57d3429701c8c583ecae8dfc
Aln1ko <cklomka@gmail.com>
2021-12-25 13:55:23 +0200

    second try

--- src/Game/ChangeAtributs.java
+++ src/Game/ChangeAtributs.java
@@ -2,31 +2,31 @@ package Game;
 
 interface InterfaceChangeAtributs
 {
-    public static void change_money(character hero,int money){}
+    public static void change_money(Character hero,int money){}
 
-    public static void change_marks(character hero,int marks){}
+    public static void change_marks(Character hero,int marks){}
 
-    public static void change_moral_points(character hero,int moral_points){}
+    public static void change_moral_points(Character hero,int moral_points){}
 
 }
 
 public class ChangeAtributs implements  InterfaceChangeAtributs
 {
-    public void change_money(character hero, int money)
+    public void change_money(Character hero, int money)
     {
         int my_money = hero.get_money();
         my_money += money;
         hero.set_money(my_money);
     }
 
-    public void change_marks(character hero,int marks)
+    public void change_marks(Character hero,int marks)
     {
         int my_marks = hero.get_marks();
         my_marks += marks;
         hero.set_marks(my_marks);
     }
 
-    public void change_moral_points(character hero,int moral_points)
+    public void change_moral_points(Character hero,int moral_points)
     {
         int my_moral_points = hero.get_moral_points();
         my_moral_points += moral_points;
--- src/Game/game.java
+++ src/Game/game.java
@@ -1,6 +1,5 @@
 package Game;
 
-
 public class game
 {
 
